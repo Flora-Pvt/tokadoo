ee9ee8a1be58b28e11c6cb50e30f2fde26fa0a67
Flora-Pvt <f.preuvot@live.fr>
2021-01-21 10:00:56 -0500

    navbar styled

--- src/parts/navbar.tsx
+++ src/parts/navbar.tsx
@@ -4,17 +4,23 @@ function Navbar() {
   const currentUserId = 1;
 
   return (
-    <>
-      <Link to="/" aria-label="link to home with the new lists">
-        <p>Lists</p>
-      </Link>
-      <Link to="/users" aria-label="link to users page">
-        <p>See my friends</p>
-      </Link>
-      <Link to={"/profil/" + currentUserId}aria-label="link to my page">
-        <p>Check my profil</p>
-      </Link>
-    </>
+    <nav className="navbar">
+
+      <h1 className="navbar_title" >TOKADOO</h1>
+
+      <section className="navbar_links" >
+        <Link to="/" aria-label="link to home with the new lists">
+          <p className="navbar_links_oneLink">Lists</p>
+        </Link>
+        <Link to="/users" aria-label="link to users page">
+          <p className="navbar_links_oneLink">See my friends</p>
+        </Link>
+        <Link to={"/profil/" + currentUserId} aria-label="link to my page">
+          <p className="navbar_links_oneLink">Check my profil</p>
+        </Link>
+      </section>
+
+    </nav>
   )
 }
 
