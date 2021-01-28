a15d3fde704d9817036ddd0ed6e50d7bc335c682
Flora-Pvt <f.preuvot@live.fr>
2021-01-28 15:58:35 -0500

    User entity

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,5 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
-console.log("Hello world");
+const typeorm_1 = require("typeorm");
+const User_js_1 = require("./entity/User.js");
+typeorm_1.createConnection()
+    .then(async (connection) => {
+    let savedUser = await connection.manager.find(User_js_1.User);
+    console.log("All users from the db: ", savedUser);
+})
+    .catch((error) => console.log(error));
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,25 @@
-import "reflect-metadata"
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User.js";
 
-console.log("Hello world")
+createConnection()
+  .then(async (connection) => {
+    /*let user = new User();
+    user.firstname = "Jane";
+    user.lastname = "Doe";
+    user.avatar = "photo-of-jane.jpg";
+    user.email = "janedoe@tokadoo.com";
+    user.password = "password";
+    user.adressLineOne = "a street";
+    user.adressLineTwo = "in a batiment";
+    user.city = "Montreal";
+    user.province = "Quebec";
+    user.zip = "H1K 1H1";
+
+    await connection.manager.save(user).then((user) => {
+      console.log("User has been saved. Id is", user.id);
+    });*/
+    let savedUser = await connection.manager.find(User);
+    console.log("All users from the db: ", savedUser);
+  })
+  .catch((error) => console.log(error));
