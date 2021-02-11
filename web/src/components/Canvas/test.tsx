1b3bdcc230b7886afd64f30bed2bdced1f19805c
Flora-Pvt <f.preuvot@live.fr>
2021-02-11 18:12:07 -0500

    square moving with mouse

--- web/src/components/Canvas/test.tsx
+++ web/src/components/Canvas/test.tsx
@@ -1,50 +1,65 @@
-import { useRef, useState } from "react";
-
-const SCALE = 0.5;
+import { useRef } from "react";
 
 function Animation() {
+  // const time = new Date();
   const canvas: any = useRef(null);
-  const [coordinates, setCoordinates]: any = useState([
-    { x: window.innerWidth / 2, y: window.innerHeight / 2 },
-    { x: window.innerWidth / 1.5, y: window.innerHeight / 1.5 },
-  ]);
 
-  function init() {
-    if (canvas != null) {
-      window.requestAnimationFrame(draw);
+  class Rectangle {
+    ypoint;
+    xpoint;
+
+    constructor(xpoint, ypoint) {
+      this.xpoint = xpoint;
+      this.ypoint = ypoint;
+    }
+
+    draw() {
+      if (canvas.current !== null && canvas.current !== undefined) {
+        let ctx = canvas.current.getContext("2d");
+        // clear the canvas area before rendering the coordinates held in state
+        ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+        ctx.save();
+        ctx.translate(-50, -50);
+        /*ctx.rotate(
+            ((2 * Math.PI) / 60) * time.getSeconds() +
+            ((2 * Math.PI) / 60000) * time.getMilliseconds()
+          );*/
+
+        ctx.beginPath();
+        ctx.rect(this.xpoint, this.ypoint, 100, 100);
+        ctx.closePath();
+
+        ctx.fillStyle = "lightblue";
+        ctx.lineWidth = 2;
+        ctx.fill();
+
+        ctx.restore();
+      }
+    }
+
+    moveRectangle(newXpoint) {
+      this.xpoint = newXpoint;
+      this.draw();
+    }
+
+    clickRectangle(xmouse, ymouse) {
+      const distance = Math.sqrt(
+        (xmouse - this.xpoint) * (xmouse - this.xpoint) +
+        (ymouse - this.ypoint) * (ymouse - this.ypoint)
+      );
+
+      if (distance < 70) {
+        console.log("short enough : " + distance);
+        this.moveRectangle(this.xpoint - 100)
+      }
     }
   }
 
-  const draw = () => {
-    var time = new Date();
 
-    var ctx = canvas.current.getContext("2d");
-    // clear the canvas area before rendering the coordinates held in state
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  let rectangle = new Rectangle(300, 300);
 
-    ctx.save();
-    ctx.scale(SCALE, SCALE);
-    ctx.translate(500, 500);
-    ctx.rotate(
-      ((2 * Math.PI) / 60) * time.getSeconds() +
-        ((2 * Math.PI) / 60000) * time.getMilliseconds()
-    );
-
-    ctx.beginPath();
-    ctx.moveTo(100, 500);
-    ctx.rect(100, 60, 100, 100);
-    ctx.closePath();
-
-    ctx.fillStyle = "lightblue";
-    ctx.lineWidth = 2;
-    ctx.fill();
-
-    ctx.restore();
-
-    window.requestAnimationFrame(draw);
-  };
-
-  init();
+  rectangle.draw();
 
   return (
     <canvas
@@ -52,6 +67,12 @@ function Animation() {
       width={window.innerWidth}
       height={window.innerHeight}
       className="canvas"
+      onMouseMove={(event) => {
+        const rect = canvas.current.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        rectangle.clickRectangle(x, y);
+      }}
     ></canvas>
   );
 }
