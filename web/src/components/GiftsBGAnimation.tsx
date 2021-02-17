424cdd32a383fe7c1c62299e161d10b4bbe30af3
Flora-Pvt <f.preuvot@live.fr>
2021-02-17 15:12:33 -0500

    animation is cleared at new page

--- web/src/components/GiftsBGAnimation.tsx
+++ web/src/components/GiftsBGAnimation.tsx
@@ -1,3 +1,4 @@
+import React, { Component } from "react";
 import {
   Engine,
   Render,
@@ -11,120 +12,122 @@ import {
 } from "matter-js";
 
 //Icons made by https://www.freepik.com from https://www.flaticon.com/
-import orangeGift from "../assets/images/orange-gift.svg"
-import yellowGift from "../assets/images/yellow-gift.svg"
+import orangeGift from "../assets/images/orange-gift.svg";
+import yellowGift from "../assets/images/yellow-gift.svg";
 
-function GiftsBGAnimation() {
+class GiftsBGAnimation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  // create engine
-  let engine = Engine.create(),
-    world = engine.world;
+  componentDidMount() {
+    // create engine
+    let engine = Engine.create(),
+      world = engine.world;
 
-  // create renderer
-  let render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-      width: window.innerWidth,
-      height: window.innerHeight,
-      showAngleIndicator: false,
-      wireframes: false,
-      background: "transparent"
-    },
-  });
+    // create renderer
+    let render = Render.create({
+      element: this.refs.scene,
+      engine: engine,
+      options: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        showAngleIndicator: false,
+        wireframes: false,
+        background: "transparent",
+      },
+    });
 
-  Render.run(render);
+    Render.run(render);
 
-  // create runner
-  let runner = Runner.create();
-  Runner.run(runner, engine);
+    // create runner
+    let runner = Runner.create();
+    Runner.run(runner, engine);
 
-  // add bodies
-  let offset = 10,
-    options = {
-      isStatic: true,
-      render: {
-        fillStyle: "transparent"
-      }
-    };
-
-  world.bodies = [];
-
-  // these static walls will not be rendered in this sprites example, see options
-  World.add(world, [
-    Bodies.rectangle(400, -offset, window.innerWidth, 10, options),
-    Bodies.rectangle(400, 600, window.innerWidth, 10, options),
-    Bodies.rectangle((window.innerWidth - 510) + offset, 300, 10, window.innerHeight, options),
-    Bodies.rectangle(-235, 300, 10, window.innerHeight, options)
-  ]);
-
-  let stack = Composites.stack(0, 0, 10, 4, 0, 0, function (x, y) {
-    if (Common.random() > 0.35) {
-      return Bodies.rectangle(x, y, 64, 64, {
+    // add bodies
+    let offset = 10,
+      options = {
+        isStatic: true,
         render: {
-          strokeStyle: '#ffffff',
-          sprite: {
-            texture: yellowGift
-          }
-        }
-      });
-    } else {
-      return Bodies.circle(x, y, 35, {
-        density: 0.0005,
-        frictionAir: 0.06,
-        restitution: 0.3,
-        friction: 0.01,
-        render: {
-          sprite: {
-            texture: orangeGift
-          }
-        }
-      });
-    }
-  });
+          fillStyle: "transparent",
+        },
+      };
 
-  World.add(world, stack);
+    world.bodies = [];
 
-  // add mouse control
-  let mouse = Mouse.create(render.canvas),
-    mouseletraint = MouseConstraint.create(engine, {
-      mouse: mouse,
-      letraint: {
-        stiffness: 0.2,
-        render: {
-          visible: false
-        }
+    // these static walls will not be rendered in this sprites example, see options
+    World.add(world, [
+      Bodies.rectangle(400, -offset, window.innerWidth, 10, options),
+      Bodies.rectangle(400, 600, window.innerWidth, 10, options),
+      Bodies.rectangle(
+        window.innerWidth - 510 + offset,
+        300,
+        10,
+        window.innerHeight,
+        options
+      ),
+      Bodies.rectangle(-235, 300, 10, window.innerHeight, options),
+    ]);
+
+    let stack = Composites.stack(0, 0, 10, 4, 0, 0, function (x, y) {
+      if (Common.random() > 0.35) {
+        return Bodies.rectangle(x, y, 64, 64, {
+          render: {
+            strokeStyle: "#ffffff",
+            sprite: {
+              texture: yellowGift,
+            },
+          },
+        });
+      } else {
+        return Bodies.circle(x, y, 35, {
+          density: 0.0005,
+          frictionAir: 0.06,
+          restitution: 0.3,
+          friction: 0.01,
+          render: {
+            sprite: {
+              texture: orangeGift,
+            },
+          },
+        });
       }
     });
 
-  World.add(world, mouseletraint);
+    World.add(world, stack);
 
-  // keep the mouse in sync with rendering
-  render.mouse = mouse;
+    // add mouse control
+    let mouse = Mouse.create(render.canvas),
+      mouseletraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        letraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false,
+          },
+        },
+      });
 
-  // fit the render viewport to the scene
-  Render.lookAt(render, {
-    min: { x: 0, y: 0 },
-    max: { x: 800, y: 600 }
-  });
+    World.add(world, mouseletraint);
 
-  // context for MatterTools.Demo
-  return {
-    engine: engine,
-    runner: runner,
-    render: render,
-    canvas: render.canvas,
-    stop: function () {
-      World.clear(world);
-      Engine.clear(engine);
-      Render.stop(render);
-      Runner.stop(runner);
-      render.canvas.remove();
-      render.canvas = null;
-      render.context = null;
-      render.textures = {};
-    }
-  };
+    // keep the mouse in sync with rendering
+    render.mouse = mouse;
+
+    // fit the render viewport to the scene
+    Render.lookAt(render, {
+      min: { x: 0, y: 0 },
+      max: { x: 800, y: 600 },
+    });
+
+    this.setState({
+      render: render,
+    });
+  }
+
+  render() {
+    return <div ref="scene" />;
+  }
 }
 
 export default GiftsBGAnimation;
