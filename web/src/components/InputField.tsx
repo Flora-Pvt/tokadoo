85f6300354f173a5def9f8c9615cf4cbf0704cb5
Flora-Pvt <f.preuvot@live.fr>
2021-02-18 12:35:36 -0500

    refactor login with formik

---
+++ web/src/components/InputField.tsx
@@ -0,0 +1,22 @@
+import React, { InputHTMLAttributes } from "react";
+import { useField } from "formik";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  textarea?: boolean;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  size: _,
+  ...props
+}) => {
+
+  const [field, { error }] = useField(props);
+
+  return (
+    <>
+      <input {...field} {...props} className="form__input" />
+      {error ? <span className="form__error">{error}</span> : null}
+    </>
+  );
+};
--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,110 +1,75 @@
-import { useState, useRef, Dispatch, SetStateAction } from "react";
-import { Link } from "react-router-dom";
-import { gsap } from "gsap";
-
+import { useRef } from "react";
+import { Link } from "react-router-dom"
+import { Formik, Form } from "formik";
+import { InputField } from "../components/InputField";
 import { useLoginMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
+
+import { gsap } from "gsap";
 import GiftsBGAnimation from "../components/GiftsBGAnimation";
 
 function Login(): JSX.Element {
-  const [fields, setFields]: [
-    { email: string; password: string },
-    Dispatch<SetStateAction<{ email: string; password: string }>>
-  ] = useState({ email: "", password: "" });
-  const [errors, setErrors]: [{}, Dispatch<SetStateAction<{}>>] = useState({});
-  const [, login] = useLoginMutation();
-
   const validationMessage = useRef(null);
   gsap.set(validationMessage.current, { yPercent: 0 });
 
-  const handleChange = (field, event) => {
-    fields[field] = event.target.value;
-    setFields(fields);
-  };
-
-  const handleValidation = () => {
-    setFields(fields);
-    let errors = {};
-    let formValidity = true;
-    setErrors(errors);
-    return formValidity;
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (handleValidation()) {
-      setErrors("");
-
-      const response = await login(fields);
-      if (response.data?.login.errors) {
-        setErrors(toErrorMap(response.data.login.errors));
-      } else if (response.data?.login.user) {
-        let tl = gsap.timeline({ paused: true });
-        tl.from(validationMessage.current, { yPercent: 0 });
-        tl.to(validationMessage.current, { yPercent: -100 });
-        tl.play();
-
-        const relocation = () => {
-          window.location.href = "/lists";
-        };
-        setTimeout(relocation, 3000);
-      }
-    }
-  };
+  const [, login] = useLoginMutation();
 
   return (
     <main className="wrapper">
+
       <GiftsBGAnimation />
 
-      <form
-        encType="multipart/form-data"
-        onSubmit={(e) => handleSubmit(e)}
-        noValidate
-        className="form"
+      <Formik
+        initialValues={{ email: "", password: "" }}
+
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login(values);
+
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            
+            let tl = gsap.timeline({ paused: true });
+            tl.from(validationMessage.current, { yPercent: 0 });
+            tl.to(validationMessage.current, { yPercent: -100 });
+            tl.play();
+
+            const relocation = () => {
+              window.location.href = "/lists";
+            };
+            setTimeout(relocation, 3000);
+          }
+        }}
       >
-        <h2>Connexion</h2>
+        {() => (
+          <Form className="form">
+            <InputField name="email" placeholder="email" />
 
-        <input
-          required
-          name="email"
-          type="email"
-          placeholder="Mail"
-          value={fields["email"]}
-          onChange={(e) => handleChange("email", e)}
-          className="form__input"
-        />
-        <span className="form__error">{errors["email"]}</span>
+            <InputField
+              name="password"
+              placeholder="password"
+              type="password"
+            />
 
-        <input
-          required
-          name="password"
-          type="password"
-          placeholder="Mot de passe"
-          value={fields["password"]}
-          onChange={(e) => handleChange("password", e)}
-          className="form__input"
-        />
-        <span className="form__error">{errors["password"]}</span>
-
-        <button
-          type="submit"
-          onClick={(e) => handleSubmit(e)}
-          className="form__button"
-        >
-          Connecte toi
-        </button>
-        <p>
-          Tu n'as pas de compte ? Enregistre toi{" "}
-          <Link to="/signup" className="form__link">
-            ici
+            <button
+              type="submit"
+              className="form__button"
+            >
+              register
+            </button>
+          </Form>
+        )}
+      </Formik>
+      <p>
+        Tu as déjà un compte ? Connecte toi{" "}
+        <Link to="/login" className="form__link">
+          ici
           </Link>
-        </p>
-        <section className="form__verso" ref={validationMessage}>
-          <h2 className="form__verso__title">Parfait !</h2>
-          <p>{"Ravie de te revoir :)"}</p>
-        </section>
-      </form>
+      </p>
+      <section className="form__verso" ref={validationMessage}>
+        <h2 className="form__verso__title">Parfait !</h2>
+        <p>Tu peux maintenant créer ta première liste !</p>
+      </section>
     </main>
   );
 }
